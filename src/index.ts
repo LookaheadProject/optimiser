03b41330bfcc47a3636dfb9d7f2d6b8119cfb509
Christiaan-van-Aardt <89824373+Christiaan-van-Aardt@users.noreply.github.com>
2024-06-25 10:52:28 +1000

    Added interfaces and classes based on parsing schema to structures. Added relevant structure examples to index.

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,79 @@
-import { Class, Subject, Timetable, Day, ClassType } from "./structures";
+import {
+  Subject,
+  ActivityGroup,
+  Stream,
+  Activity,
+  ActivityType,
+  Day,
+} from "./structures";
 
-console.log("Hello world!")
+// The following are one example of each of the main structures from the schema
 
-let subjects: Subject[] = [
-  {
-    name: "Subject1",
-    classes: {
-      "Tutorial1": [
-        {
-          start: [14, 0],
-          end: [15, 0],
-          day: Day.Mon,
-          type: ClassType.Tutorial,
-          name: "Tutorial1.1"
-        },
-        {
-          start: [14, 0],
-          end: [15, 0],
-          day: Day.Mon,
-          type: ClassType.Tutorial,
-          name: "Tutorial1.2"
-        }
-      ],
-      "Tutorial2": [
-        {
-          start: [14, 0],
-          end: [15, 0],
-          day: Day.Wed,
-          type: ClassType.Tutorial,
-          name: "Tutorial2.1"
-        },
-        {
-          start: [18, 0],
-          end: [19, 0],
-          day: Day.Wed,
-          type: ClassType.Tutorial,
-          name: "Tutorial2.2"
-        }
-      ]
-    }
-  }
-];
+// Example Activity
+let statsActivity = {
+  activity_type: ActivityType.Lecture,
+  name: "Lecture 1",
+  activty_id: 1,
+  weeks: [],
+  day: Day.Mon,
+  times: [
+    [9, 0],
+    [10, 0],
+  ],
+  location: "PAR-160-G-G01-JH Michell Theatre (308)",
+};
 
-let classes: Class[] = [subjects[0].classes["Tutorial1"][0]];
+// Example Stream with one acitivty
+let statsStream = {
+  stream_id: 1,
+  activity_list: [statsActivity],
+};
 
+// Example ActivityGroup with one stream
+let statsActivityGroup = {
+  name: "Lecture",
+  group_id: 1,
+  stream_list: [statsStream],
+};
 
+// Example Subject with one ActivityGroup
+let Statistics = {
+  code: "MAST20005",
+  name: "Statistics",
+  year: "2024",
+  offering: "Semester 2",
+  activity_groups: [statsActivityGroup],
+};
 
-let t = new Timetable(classes, subjects);
-console.log(t);
+console.log(statsActivity);
+console.log(statsStream);
+console.log(statsActivityGroup);
+console.log(Statistics);
+
+// Constructor Examples
+
+let Activity1 = new Activity(
+  ActivityType.Lecture,
+  "Lecture 1",
+  1,
+  [],
+  Day.Mon,
+  [
+    [9, 0],
+    [10, 0],
+  ],
+  "PAR-160-G-G01-JH Michell Theatre (308)"
+);
+
+let Stream1 = new Stream(1, [Activity1]);
+
+let ActivityGroup1 = new ActivityGroup("Lecture", 1, [Stream1]);
+
+let Subject1 = new Subject("MAST20005", "Statistics", 2024, "Semester 2", [
+  ActivityGroup1,
+]);
+
+console.log(Activity1);
+console.log(Stream1);
+console.log(ActivityGroup1);
+console.log(Subject1);
--- src/structures.ts
+++ src/structures.ts
@@ -5,48 +5,140 @@ export enum Day {
   Thu,
   Fri,
   Sat,
-  Sun
-};
+  Sun,
+}
 
-export enum ClassType {
+export enum ActivityType {
   Lecture,
-  Tutorial
+  Tutorial,
 }
 
 // Time, specified in 24-hour time
 export type Time = [hour: number, minute: number];
 
-export interface Class {
-  start: Time,
-  end: Time,
-  day: Day,
-  type: ClassType,
-  name: string
-};
+// A start and end Time pair
+type Times = [start: Time, end: Time];
 
-export interface Subject {
-  name: string,
-  classes: {
-    [index: string]: Class[]
+export interface IActivity {
+  activity_type: ActivityType;
+  name: string;
+  activity_id: number;
+  weeks: number[];
+  day: Day;
+  times: Times;
+  location: string;
+}
+
+export class Activity implements IActivity {
+  activity_type: ActivityType;
+  name: string;
+  activity_id: number;
+  weeks: number[];
+  day: Day;
+  times: Times;
+  location: string;
+
+  constructor(
+    activity_type: ActivityType,
+    name: string,
+    activity_id: number,
+    weeks: number[],
+    day: Day,
+    times: Times,
+    location: string
+  ) {
+    this.activity_type = activity_type;
+    this.name = name;
+    this.activity_id = activity_id;
+    this.weeks = weeks;
+    this.day = day;
+    this.times = times;
+    this.location = location;
   }
 }
 
-interface ITimetable {
-  classes: Class[],
-  subjects: Subject[]
+export interface IStream {
+  stream_id: number;
+  activity_list: Activity[];
 }
 
-export class Timetable implements ITimetable {
-  classes: Class[];
+export class Stream implements IStream {
+  stream_id: number;
+  activity_list: Activity[];
+
+  constructor(stream_id: number, activity_list: Activity[]) {
+    this.stream_id = stream_id;
+    this.activity_list = activity_list;
+  }
+}
+
+export interface IActivityGroup {
+  name: string;
+  group_id: number;
+  stream_list: Stream[];
+}
+
+export class ActivityGroup implements IActivityGroup {
+  name: string;
+  group_id: number;
+  stream_list: Stream[];
+
+  constructor(name: string, group_id: number, stream_list: Stream[]) {
+    this.name = name;
+    this.group_id = group_id;
+    this.stream_list = stream_list;
+  }
+}
+
+export interface ISubject {
+  code: string;
+  name: string;
+  year: number;
+  offering: string;
+  activity_groups: ActivityGroup[];
+}
+
+export class Subject implements ISubject {
+  code: string;
+  name: string;
+  year: number;
+  offering: string;
+  activity_groups: ActivityGroup[];
+
+  constructor(
+    code: string,
+    name: string,
+    year: number,
+    offering: string,
+    activity_groups: ActivityGroup[]
+  ) {
+    this.code = code;
+    this.name = name;
+    this.year = year;
+    this.offering = offering;
+    this.activity_groups = activity_groups;
+  }
+}
+
+// A Timetable is a collection of subjects with an allocation for one stream per
+//  ActivityGroup
+
+// This is what the evaluation function will take as an input.
+
+export interface ITimetable {
   subjects: Subject[];
+  allocation: number[][];
+}
 
-  constructor(classes: Class[], subjects: Subject[]) {
-    this.classes = classes;
-    this.subjects = subjects;
-  }
+// A preference represents the user input - very unrefined
 
-  children(): Timetable[] {
-    // iterator to return child timetables
-    return [];
-  }
+export interface IPreferences {
+  timeRestrictionStart: Time;
+  timeRestrictionEnd: Time;
+  avoidDays: number[];
+  minimiseClashes: boolean;
+  skipLectures: boolean;
+  cramClasses: boolean;
+  allocateBreaks: boolean;
+  streamedClasses: boolean;
 }
