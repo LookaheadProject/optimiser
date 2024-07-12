7378c74e01f68700122ef8371b2f1e56169438f1
Oliver Cheng <olliecheng@outlook.com>
2024-07-12 14:59:52 +1000

    Update index.ts to allow imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from "./structures";
+export * from "./optimiser";
+
 import {
   Day,
   ISubject,
@@ -6,24 +9,26 @@ import {
 
 import { optimise } from "./optimiser";
 
-// The following are one example of each of the main structures from the schema
-
-const subjects: ISubject[] = [
-  require("./example_subject_MAST20005.json")
-];
-
-const examplePreferences: IPreferences = {
-  timeRestrictionStart: [8, 30],
-  timeRestrictionEnd: [16, 30],
-  avoidDays: [Day.Fri],
-  minimiseClashes: false,
-  skipLectures: true,
-  cramClasses: false,
-  allocateBreaks: true,
-  streamedClasses: false
+if (require.main === module) {
+  // The following are one example of each of the main structures from the schema
+  
+  const subjects: ISubject[] = [
+    require("./example_subject_MAST20005.json")
+  ];
+  
+  const examplePreferences: IPreferences = {
+    timeRestrictionStart: [8, 30],
+    timeRestrictionEnd: [16, 30],
+    avoidDays: [Day.Fri],
+    minimiseClashes: false,
+    skipLectures: true,
+    cramClasses: false,
+    allocateBreaks: true,
+    streamedClasses: false
+  }
+  
+  let optimisedTimetable = optimise(subjects, examplePreferences);
+  
+  // output result with full depth
+  console.dir(optimisedTimetable, { depth: null })
 }
-
-let optimisedTimetable = optimise(subjects, examplePreferences);
-
-// output result with full depth
-console.dir(optimisedTimetable, { depth: null })
